e75a426716698e4ab914036c2692fe1775b219e1
Philippe Siguret <p.siguret@gmail.com>
2016-07-17 00:59:53 +0200

    UX App
     - Possibilité de changer la couleur du fond
     - Div en full Width-Height sur tous les devices
     - Multiples modifications CSS mineures
     - Suppression Navbar

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-  $('#display').on('click',function(){
+    $('#display').on('click', function() {
+        $(".inactiveOnClick").hide(400);
+        $.ajax({
+            url: "https://spreadsheets.google.com/feeds/list/1YNKuHfskZjAP8TbFKnBfK7oXhtmCB76rGWWBsO79ZzU/od6/public/values?alt=json",
+            success: function(data) {
+                var entries = data.feed.entry;
 
-      $.ajax({
-        url: "https://spreadsheets.google.com/feeds/list/1YNKuHfskZjAP8TbFKnBfK7oXhtmCB76rGWWBsO79ZzU/od6/public/values?alt=json",
-        success: function(data) {
-          var entries = data.feed.entry;
+                for (var i = 0; i < entries.length; i++) {
+                    var question = entries[i].gsx$question.$t;
+                    var reponse = entries[i].gsx$réponse.$t;
+                    var num = entries[i].gsx$n.$t;
+                    var color = entries[i].gsx$couleurdufond.$t;
 
+                    $('#content').append('<a href="#' + num + '"><div id="' + i + '" class="slideUp fullScreen"style="background-color:' + color + ';"><h1>' + question + '</h1><h3>' + reponse + '</h3></div></a>');
 
-          for (var i = 0 ; i < entries.length; i++) {
-            var question = entries[i].gsx$question.$t;
-            var reponse = entries[i].gsx$réponse.$t;
-            var num = entries[i].gsx$n.$t;
-
-            $('#content').append('<div class="question container-fluid" id="' + i + '""><div class="row"><div class="col-xs-8 col-xs-offset-2"><div class="vertical-spacer"></div><h1>' + question + '</h1></div></div><div class="vertical-spacer"></div>');
-            $('#content').append('<div class="question container-fluid"><div class="row"><div class="col-xs-8 col-xs-offset-2"><h3>' + reponse + '</h3><div class="vertical-spacer"></div></div></div></div>');
-            $('#content').append('<div class="question container-fluid"><div class="col-xs-8 col-xs-offset-2"><a class="btn btn-primary page-scroll" href="#' + num + '" role="button">Question suivante</a></div><div class="vertical-spacer"></div><hr></div></div>');
-          }
-        }
-      });
-  });
+                }
+            }
+        });
+    });
 });
-
