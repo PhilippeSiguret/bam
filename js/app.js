b623701f21a5381339f54a2e03a94aa92c4d882b
Philippe Siguret <p.siguret@gmail.com>
2016-07-17 14:52:53 +0200

    Ajout du tuto + modifications de variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-    $('#display').on('click', function() {
+
+    $('a').on('click', function() {
         $(".inactiveOnClick").hide(400);
+    });
+    $('#display').on('click', function() {
         $.ajax({
             url: "https://spreadsheets.google.com/feeds/list/1YNKuHfskZjAP8TbFKnBfK7oXhtmCB76rGWWBsO79ZzU/od6/public/values?alt=json",
             success: function(data) {
@@ -9,15 +12,15 @@ $(document).ready(function() {
                 for (var i = 0; i < entries.length; i++) {
                     var question = entries[i].gsx$question.$t;
                     var reponse = entries[i].gsx$réponse.$t;
-                    var num = entries[i].gsx$num.$t;
+                    var id = entries[i].gsx$id.$t;
                     var style = entries[i].gsx$style.$t;
                     var questionSuivante = entries[i].gsx$questionsuivante.$t;
-                    var questionGold = entries[i].gsx$boutonquestiongold.$t;
+                    var bouton = entries[i].gsx$bouton.$t;
 
-                    $('#content').append('<a href="#' + questionSuivante + '"><div id="' + num + '" class="slideUp fullScreen"style="' + style + '"><h1>' + question + '</h1><h3>' + reponse + '</h3>' + questionGold + '</div></a>');
+                    $('#content').append('<a href="#' + questionSuivante + '"><div id="' + id + '" class="fullScreen"style="' + style + '"><h1>' + question + '</h1><h3>' + reponse + '</h3>' + bouton + '</div></a>');
                 }
             }
         });
-
     });
+
 });
